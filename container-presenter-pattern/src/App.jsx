4d117bb590122774729e160e8ca560f939d5f393
Masud Rana <msd.rana.2008@gmail.com>
2025-10-25 23:21:33 +0600

    Title of the project is added

--- container-presenter-pattern/src/App.jsx
+++ container-presenter-pattern/src/App.jsx
@@ -4,7 +4,9 @@ import ProductListContainer from "./components/ProductListContainer";
 const App = () => {
   return (
     <div className="p-6 bg-gray-50 min-h-screen">
-      {/* <h1 className="text-3xl font-bold text-blue-800 mb-6">Container-Presenter Pattern</h1> */}
+      <h1 className="text-3xl font-bold text-blue-800 mb-6">
+        Container-Presenter Pattern
+      </h1>
       <ProductListContainer />
     </div>
   );
